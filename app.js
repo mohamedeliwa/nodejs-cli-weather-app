9367b963bbcc0393725a833ededbe8a81e7603ca
mohamedeliwa <mohamed.av11@gmail.com>
2020-02-10 20:17:18 +0200

    uncommenting request function for weather api

--- app.js
+++ app.js
@@ -5,24 +5,24 @@ const request = require("request");
 const url =
   "https://api.darksky.net/forecast/3203ccb518b4c4038e8892f6ebfd27a5/37.8267,-122.4233?units=si&lang=en";
 
-// request(
-//   {
-//     url,
-//     // json to true to make request parse the response body automatically.
-//     json: true
-//   },
-//   (err, resp, body) => {
-//     if (err) {
-//       console.log("unable to connect to weather service");
-//     } else if (body.error) {
-//       console.log(`\nUnable to find location.\nServer resp is: ${body.error}`);
-//     } else {
-//       console.log(
-//         `\n${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out, There is a ${body.currently.precipProbability}% chance of rain`
-//       );
-//     }
-//   }
-// );
+request(
+  {
+    url,
+    // json to true to make request parse the response body automatically.
+    json: true
+  },
+  (err, resp, body) => {
+    if (err) {
+      console.log("unable to connect to weather service");
+    } else if (body.error) {
+      console.log(`\nUnable to find location.\nServer resp is: ${body.error}`);
+    } else {
+      console.log(
+        `\n${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out, There is a ${body.currently.precipProbability}% chance of rain`
+      );
+    }
+  }
+);
 
 // the url of the api we are using for geocoding, https://mapbox.com
 const geocodingUrl =
